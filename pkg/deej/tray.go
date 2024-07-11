28ddd70385540d37564935fcf565924e0485f05a
Semen <95562395+semen-trololo@users.noreply.github.com>
2024-07-11 08:08:02 +0300

    Update tray.go

--- pkg/deej/tray.go
+++ pkg/deej/tray.go
@@ -1,6 +1,9 @@
 package deej
 
 import (
+	"time"
+	"fmt"
+	
 	"github.com/getlantern/systray"
 
 	"github.com/omriharel/deej/pkg/deej/icon"
@@ -22,6 +25,9 @@ func (d *Deej) initializeTray(onDone func()) {
 
 		refreshSessions := systray.AddMenuItem("Re-scan audio sessions", "Manually refresh audio sessions if something's stuck")
 		refreshSessions.SetIcon(icon.RefreshSessions)
+
+		refreshCon := systray.AddMenuItem("Re-connect", "Manually refresh connect COM port")
+		refreshCon.SetIcon(icon.RefreshSessions)
 
 		if d.version != "" {
 			systray.AddSeparator()
@@ -63,6 +69,18 @@ func (d *Deej) initializeTray(onDone func()) {
 					// performance: the reason that forcing a refresh here is okay is that users can't spam the
 					// right-click -> select-this-option sequence at a rate that's meaningful to performance
 					d.sessions.refreshSessions(true)
+				case <-refreshCon.ClickedCh:
+					logger.Info("Reconnecting the Sound Mixer")
+					d.serial.Stop()
+					time.Sleep(3 * time.Second)
+					if err := d.serial.Start(); err != nil {
+						d.notifier.Notify(fmt.Sprintf("Failed to connect %s!", d.config.ConnectionInfo.COMPort),
+							"This serial port doesn't exist, check your configuration and make sure it's set correctly.")
+						logger.Warnw("Failed to connect", "error", err)
+
+					} else {
+						d.notifier.Notify(fmt.Sprintf("The sound Mixer is connected"), "")
+					}
 				}
 			}
 		}()
